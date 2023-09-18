88295efce6e821c1552a19d5891dc40f3e895d15
Harry Zhao <wpzhao@cs.ualberta.ca>
2023-09-18 11:41:27 -0600

    new changes

--- testing.py
+++ testing.py
@@ -1,3 +1,6 @@
 import requests
 print(requests.__version__)
 print(requests.get("http://google.com"))
+
+res = requests.get("https://raw.githubusercontent.com/harryz6560/lab1/master/testing.py", ) 
+print(res.text)
